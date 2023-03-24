a96018403ee45b9437ed141837255c8000e64df2
LilFatFrank <karan.parsnani@ymail.com>
2023-03-24 21:23:02 +0530

    adding highlight and expand all, collapse all button

--- src/components/FileTree.tsx
+++ src/components/FileTree.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
   faFolder,
@@ -14,7 +14,14 @@ export interface TreeNodeProps {
   children?: TreeNodeProps[]
 }
 
-const TreeNode: React.FC<TreeNodeProps> = ({ name, kind, children }) => {
+const TreeNode: React.FC<
+  TreeNodeProps & {
+    selected: string
+    setSelected: (path: string) => void
+    path: string
+    globalExpand: boolean
+  }
+> = ({ name, kind, children, selected, setSelected, path, globalExpand }) => {
   const [isOpen, setIsOpen] = useState(false)
   const hasChildren = kind === 'directory' && children && children.length > 0
   const icon = hasChildren ? (isOpen ? faFolderOpen : faFolder) : faFile
@@ -23,27 +30,64 @@ const TreeNode: React.FC<TreeNodeProps> = ({ name, kind, children }) => {
     if (hasChildren) {
       setIsOpen(!isOpen)
     }
+    setSelected(path)
   }
+
+  const isSelected = selected === path
+
+  useEffect(() => {
+    setIsOpen(globalExpand)
+  }, [globalExpand])
 
   return (
     <div>
-      <div className="tree-node" onClick={handleClick}>
+      <div
+        className={`tree-node${isSelected ? ' selected' : ''}`}
+        onClick={handleClick}
+      >
         <FontAwesomeIcon icon={icon} />
         <span>{name}</span>
       </div>
-      {isOpen && hasChildren && (
+      {isOpen && hasChildren ? (
         <div className="tree-children">
           {children.map((child, index) => (
-            <TreeNode key={index} {...child} />
+            <TreeNode
+              key={index}
+              {...child}
+              selected={selected}
+              setSelected={setSelected}
+              path={`${path}/${child.name}`}
+              globalExpand={globalExpand}
+            />
           ))}
         </div>
-      )}
+      ) : null}
     </div>
   )
 }
 
 const FileTree: React.FC<{ data: TreeNodeProps }> = ({ data }) => {
-  return <TreeNode {...data} />
+  const [selected, setSelected] = useState('')
+  const [globalExpand, setGlobalExpand] = useState(false)
+
+  const handleExpandCollapse = () => {
+    setGlobalExpand(!globalExpand)
+  }
+
+  return (
+    <div>
+      <button onClick={handleExpandCollapse}>
+        {globalExpand ? 'Collapse All' : 'Expand All'}
+      </button>
+      <TreeNode
+        {...data}
+        selected={selected}
+        setSelected={setSelected}
+        path={data.name}
+        globalExpand={globalExpand}
+      />
+    </div>
+  )
 }
 
 export default FileTree
